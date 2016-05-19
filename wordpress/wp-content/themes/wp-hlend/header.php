beb527905169a32bdde0bfa1b642b7570faefd0c
Oleg Gordiyenko <oleg.gordey777@gmail.com>
2016-05-19 17:50:40 +0300

    wp footer product header

--- wordpress/wp-content/themes/wp-hlend/header.php
+++ wordpress/wp-content/themes/wp-hlend/header.php
@@ -9,6 +9,12 @@
 
   <link href="http://www.google-analytics.com/" rel="dns-prefetch"><!-- dns prefetch -->
 
+<link media="all" rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/libs.css">
+<link media="all" rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/mvid-common.css">
+<link media="all" rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/mvid-blocks.css">
+<link media="all" rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/mvid-pages.css">
+<link media="print" rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/mvid-print.css">
+
   <!-- icons -->
   <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
 
@@ -19,30 +25,241 @@
   <![endif]-->
   <!-- css + javascript -->
   <?php wp_head(); ?>
+
+<html class="js flash history localstorage sessionstorage mediaqueries no-touchevents cssscrollbar" xml:lang="ru-RU" lang="ru-RU">
+<!--<![endif]-->
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+    <?php wp_head(); ?>
+
+
 </head>
-<body <?php body_class(); ?>>
-<!-- wrapper -->
-<div class="wrapper">
-  <header role="banner">
-    <div class="inner">
 
-      <div class="logo">
-        <?php if ( is_front_page() && is_home() ){ } else { ?>
-          <a href="<?php echo home_url(); ?>">
-            <?php  } ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
-            <?php if ( is_front_page() && is_home() ){
-            } else { ?>
-          </a>
-        <?php } ?>
-      </div><!-- /logo -->
+<body class="productDetails no-inner-shaddow">
+  <div class="wrapper">
 
-      <nav class="nav" role="navigation">
-        <?php wpeHeadNav(); ?>
-      </nav><!-- /nav -->
+    <div class="page-content" style="left: 0px;">
+      <div class="sidebar">
+      </div>
+      <div class="header-main closed" data-init="headerRegions">
+        <div class="header-area">
+          <div id="header-regions"></div>
+          <div class="region-popup">
+            <div id="region-popup" data-init="custom-tooltip" data-init-param="#region-popup, bottom" data-tooltip-text="" class="region-popup-btn" data-original-title="" title=""></div>
+          </div>
+          <div class="header-top-bar">
+            <div class="header-section">
+              <div class="header-stores-section">
+                <div class="header-stores">
+                  <a href="http://www.mvideo.ru/products/televizor-samsung-ultra-hd-ue55ju7500u-10007781#" data-init="openCloseScroll" data-init-param=".header-stores-dropdown,.header-stores-content" data-post-load-url="/sitebuilder/blocks/storeSelection.json.jsp" class="header-store-addresses-link inactive">Адреса магазинов
+              <i class="font-icon header-stores-icon"></i>
+            </a>
+                  <div class="header-stores-dropdown "></div>
+                </div>
+              </div>
+              <div class="header-info-section">
+                <strong class="header-phone">(495) 777-777-5</strong> <a class="header-help-link" href="http://www.mvideo.ru/help">Вопросы-ответы</a>
+                <a href="http://www.mvideo.ru/products/televizor-samsung-ultra-hd-ue55ju7500u-10007781#header-regions" data-post-load-url="/sitebuilder/blocks/regionSelection.json.jsp?pageId=productDetailsPage&amp;pageUrl=/products/televizor-samsung-ultra-hd-ue55ju7500u-10007781&amp;" class="header-sities-link region-opener">Москва
+            <i class="font-icon header-sities-icon"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="header-main-area">
+            <div class="header-section">
+              <div class="header-area-center">
+                <div class="header-area-holder">
+                  <a href="http://www.mvideo.ru/" class="header-logo font-icon icon-logo-main">
+                    <img class="header-logo-img" src="<?php echo get_template_directory_uri(); ?>/img/mvideo-logo.png" alt="М.Видео, нам не всё равно">
+                    <strong>М.Видео, нам не всё равно</strong>
+                  </a>
+                </div>
+              </div>
+              <div class="header-area-left">
+                <a href="http://www.mvideo.ru/products/televizor-samsung-ultra-hd-ue55ju7500u-10007781#" class="header-menu-btn">Menu</a>
 
-    </div><!-- /.inner -->
-  </header><!-- /header -->
+                <div class="header-login">
 
-  <section role="main">
-    <div class="inner">
+                </div>
+
+
+              </div>
+
+              <div class="header-area-right">
+
+
+                <div id="js-mini-basket" class="mini-basket-block ">
+                  <a href="http://www.mvideo.ru/cart" data-init="miniBasketBtn" data-url="/sitebuilder/blocks/minibasket/miniBasketNavigation.json.jsp" class="mini-basket-service-blocks mini-basket-link collapsed close-cart">
+                    <strong class="icon-trolley-cart font-icon">
+            <span class="mini-basket-amount">1</span>
+        </strong>
+                  </a>
+                  <div id="mini-basket" class="mini-basket-content">
+                    <div class="mini-basket-main-content">
+                      <div class="mini-basket-scrolling-panel">
+                        <form id="mini-basket-form" action="http://www.mvideo.ru/sitebuilder/blocks/cart/cart-mini.json.jsp?_DARGS=/sitebuilder/blocks/cart/fragments/mini-basket-filled.jsp" method="POST">
+                          <ul class="mini-basket-product-details">
+                            <li class="mini-basket-product">
+                              <div class="mini-basket-product-image">
+                                <div class="mini-basket-picture-holder">
+                                  <a href="http://www.mvideo.ru/products/radioupravlyaemaya-mashina-pilotage-buggy-stem-12-rd-ep-rc17514-40063842">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/img/40063842s.jpg" alt="Радиоуправляемая машина Pilotage Buggy Stem 12 RD EP RC17514" class="">
+                                  </a>
+                                </div>
+                              </div>
+                              <div class="mini-basket-product-description">
+                                <h4 class="mini-basket-product-name">
+        <a href="http://www.mvideo.ru/products/radioupravlyaemaya-mashina-pilotage-buggy-stem-12-rd-ep-rc17514-40063842">Радиоуправляемая машина Pilotage Buggy Stem 12 RD EP RC17514</a> </h4> </div>
+                            </li>
+                          </ul>
+                          <div style="display: none">
+
+                            <input id="update-mini-basket-button" name="/atg/commerce/order/purchase/CartModifierFormHandler.setOrderByCommerceId" type="submit">
+
+                            <input id="remove-mini-basket-button" name="/atg/commerce/order/purchase/CartModifierFormHandler.removeItemFromOrder" type="submit">
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                    <div class="mini-basket-buttons">
+                      <div class="mini-basket-action-btns">
+                        <div class="mini-basket-total-block">
+                          <strong class="mini-basket-total">Всего к оплате (р.)</strong>
+                          <strong class="mini-basket-total-price">6490</strong>
+                        </div>
+
+                        <div class="mini-basket-buttons-holder" data-pushable="true" data-action="click" data-holder="#gtmMiniBasket">
+                          <a class="btn btn-fluid mini-basket-view-basket-button" href="http://www.mvideo.ru/cart">Перейти в корзину</a>
+                        </div>
+                      </div>
+                    </div>
+                    <a href="http://www.mvideo.ru/exchange">
+                      <div class="slider-pict">
+                        <div class="slider-pict-center">
+                          <div class="lazy-load-image-holder">
+                            <img data-original="img/ex_ret.gif" alt="Гарантия и обмен по чеку" class="lazy " src="<?php echo get_template_directory_uri(); ?>/img/stub.gif">
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div class="wishlist-block">
+                  <a href="http://www.mvideo.ru/wish-list" data-device="true" class="wishlist-link default">
+                    <strong class="icon-heart font-icon"></strong>
+                  </a>
+                </div>
+                <form id="main-search-form" action="http://www.mvideo.ru/action/search?_DARGS=/sitebuilder/blocks/search/search.jsp" class="header-search-form-section" method="post" novalidate="novalidate">
+                  <div data-init="autocompleteHandler" class="header-search-form" data-init-param="{&quot;element&quot;: &quot;#frm-search-text&quot;,&quot;submitAfter&quot;: &quot;true&quot;,&quot;timeout&quot;:0}">
+                    <input placeholder="Поиск" class="header-search-input" autocomplete="off" type="text" id="frm-search-text" data-init-param=".header-area-right #main-search-form" data-msg-required="Введено менее 2 символов" data-validate-keyup="true" data-rule-minlength="2" maxlength="64" name="Ntt" value="" data-init="validationHandler" data-msg-minlength="Введено менее 2 символов" data-rule-required="true">
+                    <span class="font-icon icon-zoom header-search-icon">
+          <input type="submit" class="header-search-btn" value="">
+        </span>
+                  </div>
+                  <div class="popup default inactive" style="display: none;">
+                    <ul class="search-list"></ul>
+                  </div>
+                </form>
+                <label for="frm-search-text" data-init="autocompleteForm" data-init-param="{&quot;element&quot;: &quot;#frm-search-text&quot;, &quot;setPopupFocus&quot;: true }" class="header-search-form-label inactive" data-device="true">
+                  <i class="font-icon icon-zoom"></i>
+                </label>
+                <div class="dimSearchSuggContainer"></div>
+              </div>
+            </div>
+          </div>
+
+          <div class="header-nav-wrap">
+            <nav class="header-nav" data-init="mobileNavigation" data-device="true">
+              <ul data-tpl="navigation" class="header-section header-nav-list">
+                <li class="header-nav-item has-dropdown hni-actions">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/vse-akcii"><span class="header-nav-item-text">Акции</span></a>
+                </li>
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/televizory-i-video"><span class="header-nav-item-text">Телевизоры, видео и Hi-Fi</span></a>
+                </li>
+
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/audiotehnika"><span class="header-nav-item-text">Аудио техника</span></a>
+                </li>
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/noutbuki-planshety-i-kompyutery"><span class="header-nav-item-text">Ноутбуки, планшеты и компьютеры</span></a>
+                </li>
+
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/telefony"><span class="header-nav-item-text">Телефоны</span></a>
+
+                </li>
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/fotoapparaty-i-videotehnika"><span class="header-nav-item-text">Фото и видео</span></a>
+                </li>
+
+
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/tehnika-dlya-doma"><span class="header-nav-item-text">Техника для дома</span></a>
+                </li>
+
+
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/tehnika-dlya-kuhni"><span class="header-nav-item-text">Техника для кухни</span></a>
+                </li>
+
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/vstraivaemaya-tehnika"><span class="header-nav-item-text">Встраиваемая техника</span></a>
+                </li>
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/krasota-i-zdorove"><span class="header-nav-item-text">Красота и здоровье</span></a>
+                </li>
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/avtomobilnaya-elektronika-i-gps"><span class="header-nav-item-text">Авто техника</span></a>
+                </li>
+
+
+                <li class="header-nav-item has-dropdown ">
+                  <a class="header-nav-item-link" href="http://www.mvideo.ru/igry-i-razvlecheniya"><span class="header-nav-item-text">Игры и развлечения</span></a>
+                </li>
+
+
+              </ul>
+
+
+            </nav>
+          </div>
+
+
+          <div class="autocomplete inactive" data-init="autocompleteHandler" data-init-param="{&quot;element&quot;: &quot;#alt-search-input&quot;,&quot;submitAfter&quot;: true,&quot;placement&quot;:&quot;append&quot;, &quot;timeout&quot;: 0}" style="display: none;">
+            <form action="http://www.mvideo.ru/action/search?_DARGS=/sitebuilder/blocks/search/search.jsp" id="alt-search-form" class="header-search-form-section" novalidate="novalidate">
+              <div class="header-search-form" data-init="validationHandler" data-init-param=".autocomplete .header-search-form-section">
+                <input id="alt-search-input" class="header-search-input" name="Ntt" type="text" data-rule-minlength="2" data-msg-minlength="Введено менее 2 символов" data-rule-required="true" data-msg-required="Введено менее 2 символов" data-ignore-change="true" data-validate-keyup="true" autocomplete="off"> <span class="font-icon icon-zoom header-search-icon">     <input class="header-search-btn" type="submit" value="Поиск">    </span> </div>
+            </form>
+            <div class="popup default inline inactive" style="display: none;">
+              <ul class="search-list"></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
